a7175bd4bd92153dbf5372591fccaff0e7e7c167
Laurent Benazet <benazetlaurent@gmail.com>
2024-08-14 18:55:47 +0200

    Init spring

---
+++ src/test/java/laurent/spring_elastic/TestSpringElasticApplication.java
@@ -0,0 +1,11 @@
+package laurent.spring_elastic;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestSpringElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(SpringElasticApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
