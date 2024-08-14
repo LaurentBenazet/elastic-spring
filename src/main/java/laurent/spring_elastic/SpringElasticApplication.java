cc3713896a8bdba39e736bd2ec376f0633f387a8
Laurent Benazet <benazetlaurent@gmail.com>
2024-08-14 19:25:43 +0200

    First route

--- src/main/java/laurent/spring_elastic/SpringElasticApplication.java
+++ src/main/java/laurent/spring_elastic/SpringElasticApplication.java
@@ -2,12 +2,23 @@ package laurent.spring_elastic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringElasticApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringElasticApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringElasticApplication.class, args);
+    }
+
+    @RequestMapping(value = "/messages", method = RequestMethod.GET)
+    @ResponseBody
+    public String getMessage() {
+        return "Coucou";
+    }
 
 }
